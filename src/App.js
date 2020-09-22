078e28cf8f362cb868f7bac9567299e64d2ba65f
Leonardo Hacklaender Wagner <nortonhw@gmail.com>
2020-09-21 21:59:47 -0300

    Methods done again

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
     const response = await api.post('repositories', {
       title: `Novo Repositorio ${Date.now()}`,
       url: "www.terra.com.br",
-      techs: [ "Juca", "Teste", "PUTA" ],
+      techs: [ "Juca", "Teste" ],
       likes: 0
     })
 
